b9d2711b2ee9fe5b4600066f611c432f79f11ae0
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-16 15:40:56 +0300

    Добавьте файлы проекта.

---
+++ lab_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        public enum Figures
+        {
+            Romb,
+            Circle,
+            Rect
+        }
+        public struct Fdata
+        {
+            public int x0, y0; // координаты
+            public int color; // цвет фигуры
+            public Figures type; //тип фигуры
+        }
+        static void Main(string[] args)
+        {
+           var Figure =  new Fdata()
+            {
+                x0 = 1,
+                y0 = 1,
+                color = 0x2727FF,
+                type = Figures.Romb
+            };
+            Console.WriteLine(Figure);
+        }
+    }
+}
