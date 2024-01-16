9ab7de3b4d1172416784908d98675c97244e1ec0
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-16 16:07:09 +0300

    2.1

---
+++ lab_2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            int sum = 0;
+
+            if(args.Length > 0)
+            {
+                for (int i = 0; i <= args.Length - 1; i ++ )
+                {
+                    if(int.TryParse(args[i], out var number))
+                    {
+                        nums.Add(number);
+                        sum += number;
+                        Console.WriteLine(number);
+                    }
+                }
+                Console.WriteLine($"Сумма чисел: {sum}");
+            }
+        }
+    }
+}
---
+++ lab_2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace lab_2._2
+{
+    class Program
+    {
+        public static string intToBinary(int num)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int index = 0; index <= 31; index++)
+            {
+                stringBuilder.Append(num & 1);
+                num >>= 1;
+                if (index != 31 && (index + 1) % 8 == 0)
+                {
+                    stringBuilder.Append(" ");
+                }
+            }
+            return stringBuilder.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Console.WriteLine(num);
+            Console.WriteLine(intToBinary(num));
+        }
+    }
+}
